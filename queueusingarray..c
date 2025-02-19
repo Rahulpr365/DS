9c0a6a3a9bccebb6982c8c97f0e5c5d3a3f599bf
Rahulpr365 <sparksmediaofficial@gmail.com>
2025-02-19 10:24:20 +0530

    Update queueusingarray..c

--- queueusingarray..c
+++ queueusingarray..c
@@ -1,57 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> #include <stdlib.h>
 
-#define MAXSIZE 5
+struct node { int data; struct node *next; };
 
-int main() {
-    int queue[MAXSIZE], ch = 1, front = -1, rear = -1, i, j = 1;
-    
-    printf("Queue using Array\n");
-    printf("\n1. Insertion \n2. Deletion \n3. Display \n4. Exit");
+struct node *newNode, *head = NULL, *temp;
 
-    while (ch) {
-        printf("\nEnter the Choice: ");
-        scanf("%d", &ch);
+void traverse() { if (head == NULL) printf("\nList is empty\n"); else { temp = head; while (temp != NULL) { printf("Data = %d\n", temp->data); temp = temp->next; } } }
 
-        switch (ch) {
-            case 1:
-                if (rear == MAXSIZE - 1) {
-                    printf("\nQueue is Full");
-                } else {
-                    printf("\nEnter no %d => ", j++);
-                    rear++;
-                    scanf("%d", &queue[rear]);
-                }
-                break;
+void insertAtFront() { int value; printf("Enter the data\n"); scanf("%d", &value); newNode = (struct node*)malloc(sizeof(struct node)); newNode->data = value; newNode->next = head; head = newNode; }
 
-            case 2:
-                if (front == rear) {
-                    printf("\nQueue is Empty");
-                } else {
-                    front++;
-                    printf("\nDeleted Element is %d", queue[front]);
-                }
-                break;
+void insertAtEnd() { int value; printf("Enter the data\n"); scanf("%d", &value); newNode = (struct node*)malloc(sizeof(struct node)); newNode->data = value; newNode->next = NULL;
 
-            case 3:
-                printf("\nQueue Elements are:\n");
-                if (front == rear) {
-                    printf("\nQueue is Empty");
-                } else {
-                    for (i = front + 1; i <= rear; i++) {
-                        printf("%d ", queue[i]);
-                    }
-                    printf("\n");
-                }
-                break;
-
-            case 4:
-                exit(0);
-
-            default:
-                printf("Wrong Choice: Please see the options");
-        }
-    }
-
-    return 0;
+if (head == NULL) {
+    head = newNode;
+    return;
 }
+
+temp = head;
+while (temp->next != NULL) {
+    temp = temp->next;
+}
+temp->next = newNode;
+
+}
+
+void insertAtPosition() { int i, pos, value; printf("Enter the data\n"); scanf("%d", &value); newNode = (struct node*)malloc(sizeof(struct node)); newNode->data = value; printf("Enter position\n"); scanf("%d", &pos);
+
+if (pos == 1) {
+    newNode->next = head;
+    head = newNode;
+    return;
+}
+
+temp = head;
+for (i = 1; i < pos - 1 && temp != NULL; i++) {
+    temp = temp->next;
+}
+
+if (temp == NULL) {
+    printf("Invalid position!\n");
+    free(newNode);
+    return;
+}
+
+newNode->next = temp->next;
+temp->next = newNode;
+
+}
+
+void deleteFirst() { if (head == NULL) printf("\nList is empty\n"); else { temp = head; head = head->next; free(temp); } }
+
+int main() { int choice; while (1) { printf("\n1. Traverse \n2. Insert at front\n3. Insert at end\n4. Insert at position\n5. Delete at first\n6. Exit\n"); printf("Enter Choice: "); scanf("%d", &choice);
+
+switch (choice) {
+        case 1:
+            traverse();
+            break;
+        case 2:
+            insertAtFront();
+            break;
+        case 3:
+            insertAtEnd();
+            break;
+        case 4:
+            insertAtPosition();
+            break;
+        case 5:
+            deleteFirst();
+            break;
+        case 6:
+            exit(0);
+        default:
+            printf("Incorrect Choice\n");
+    }
+}
+return 0;
+
+}
+
